f7bc7d16a93e3db77edf7eedd8a5f37c14206a2c
gkavithagit <Gkavitha.dev@gmail.com>
2026-09-02 22:42:30 +0530

    input and operators

---
+++ day 2 input and operators.py/input and operators.py
@@ -0,0 +1,14 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+print("My name is", name)
+print("My age is", age)
+
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
