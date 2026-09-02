d61d29f99430a1696009df88b3d59fcc0724d46e
gkavithagit <Gkavitha.dev@gmail.com>
2026-09-02 22:16:07 +0530

    day 1 variables and datatypes.py

--- day 1 hello.py
+++ day 1 hello.py
@@ -1,1 +1,16 @@
 print("Hello,Github!")
+
+name = "kavi"
+age = 19
+height = 5.3
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
