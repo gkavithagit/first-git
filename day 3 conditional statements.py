4d6ae9ba48f589094f412cf37f0277548faf075c
gkavithagit <Gkavitha.dev@gmail.com>
2026-09-03 21:59:20 +0530

    conditional statements

---
+++ day 3 conditional statements.py
@@ -0,0 +1,21 @@
+age = 20
+if age >= 18:
+  print("you are an adult")
+
+age = 16
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+mark = 75
+if mark >= 90:
+    print("A+")
+elif mark >= 75:
+    print("A")
+elif mark >= 60:
+    print("B")
+elif mark >= 40:
+    print("C")
+else:
+    print("Fail")
